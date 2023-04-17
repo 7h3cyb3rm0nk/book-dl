bbf6c1f171d5c5164aaf792b472e294a210306be
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2023-04-17 13:56:53 +0530

    Fix: length validation

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 
 
 function generate() {
-    let inp = document.getElementById("hash").value;
+    let inp = document.getElementById("hash").value.trim().toUpperCase();
     let result = document.getElementById("result");
-    if (inp === null || inp === ""){
+    if  (inp === null || inp === "" || inp.length != 32 ){
         message.textContent = "enter a valid hash value";
+        result.textContent = "";
 
         
     }
     else {
     result.textContent = `https://cdn1.booksdl.org/get.php?md5=${inp}&mirr=1`;
+    message.textContent = "";
     }
 }
