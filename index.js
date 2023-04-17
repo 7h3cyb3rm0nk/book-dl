a700f538fffa3e6f9fee4bcaff06ca153dd72d01
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2023-04-17 19:24:39 +0530

    PATCH: Added a searchbar

--- index.js
+++ index.js
@@ -14,3 +14,10 @@ function generate() {
     message.textContent = "";
     }
 }
+
+function search() {
+    let query = document.getElementById("book").value.replaceAll(" " , "+");
+    let searchQuery = `https://libgen.is/search.php?req=${query}&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def`;
+    window.open(searchQuery, '_blank');
+    
+}
